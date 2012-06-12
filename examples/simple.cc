cbd081bb677d1a106b887b261fd80e1c2f9a78ab
William Woodall <wjwwood@gmail.com>
2012-06-12 02:00:51 -0500

    Still tracking down a serial bug

--- examples/simple.cc
+++ examples/simple.cc
@@ -1,6 +1,9 @@
 #include <serial/serial.h>
 #include <iostream>
 
+#include <termios.h>
+#include <fcntl.h>
+
 using serial::Serial;
 using serial::Timeout;
 
@@ -12,23 +15,22 @@ void printHex(char *data, int length)
     printf("\n");
 }
 
-
 int main(void) {
-    Timeout t(100, 1000, 0, 1000, 0);
-    Serial s("/dev/ttyACM0", 9600, t);
+    int fd_ = ::open ("/dev/tty.usbmodemfa131", O_RDWR | O_NOCTTY | O_NONBLOCK);
 
     while (1) {
         uint8_t buffer[5000];
-        size_t bytes_read = s.read(buffer, 5000);
-        printHex((char*)buffer, bytes_read);
+        ssize_t bytes_read = ::read(fd_, buffer, 5000);
         if (bytes_read<18)
             continue;
+        std::cout << "Read " << bytes_read << " bytes." << std::endl;
+        printHex((char*)buffer, bytes_read);
         char latitude[4];
         latitude[0]=buffer[14];
         latitude[1]=buffer[15];
         latitude[2]=buffer[16];
         latitude[3]=buffer[17];
-	printHex(latitude,4);
+        printHex(latitude,4);
         int32_t latitude_int;
         memcpy(&latitude_int,latitude,4);
         std::cout << "latitude: " << latitude_int << std::endl;
@@ -37,7 +39,7 @@ int main(void) {
         longitude[1]=buffer[11];
         longitude[2]=buffer[12];
         longitude[3]=buffer[13];
-	printHex(longitude,4);
+        printHex(longitude,4);
         int32_t longitude_int;
         memcpy(&longitude_int,longitude,4);
         std::cout << "longitude: " << longitude_int << std::endl;
@@ -45,4 +47,38 @@ int main(void) {
 
     return 0;
 }
+
+// int main(void) {
+//     Timeout t(100, 1000, 0, 1000, 0);
+//     // Serial s("/dev/ttyACM0", 9600, t);
+//     Serial s("/dev/tty.usbmodemfa131", 9600, t);
+
+//     while (1) {
+//         uint8_t buffer[5000];
+//         size_t bytes_read = s.read(buffer, 5000);
+//         if (bytes_read<18)
+//             continue;
+//         printHex((char*)buffer, bytes_read);
+//         char latitude[4];
+//         latitude[0]=buffer[14];
+//         latitude[1]=buffer[15];
+//         latitude[2]=buffer[16];
+//         latitude[3]=buffer[17];
+//         printHex(latitude,4);
+//         int32_t latitude_int;
+//         memcpy(&latitude_int,latitude,4);
+//         std::cout << "latitude: " << latitude_int << std::endl;
+//         char longitude[4];
+//         longitude[0]=buffer[10];
+//         longitude[1]=buffer[11];
+//         longitude[2]=buffer[12];
+//         longitude[3]=buffer[13];
+//         printHex(longitude,4);
+//         int32_t longitude_int;
+//         memcpy(&longitude_int,longitude,4);
+//         std::cout << "longitude: " << longitude_int << std::endl;
+//    }
+
+//     return 0;
+// }
 
