4f90f01028d78e6b644832761d6d9c6b7db3e75a
chris5108 <chris5108@Latitude-linux>
2015-01-08 18:48:21 -0600

    added script to log pseudorange data

---
+++ examples/record_pseudoranges.cpp
@@ -0,0 +1,133 @@
+#include "ublox/ublox.h"
+#include <iostream>
+#include <fstream>
+
+#include "boost/filesystem.hpp"
+
+using namespace ublox;
+using namespace std;
+
+
+ofstream data_file_;  //!< file stream for logging gps data
+std::string data_filename_; //!< file name for logging gps data
+
+bool StartDataLogging(std::string filename) {
+    try {
+
+        // check to see if data directory exists, if not create it
+        boost::filesystem::path dir("data");
+
+        if (!boost::filesystem::is_directory(dir))
+            {
+            if (!boost::filesystem::create_directory(dir)) {
+                log_error_("Error creating data directory. Logging disabled.");
+                settings_.log_data_period=0;
+                return false;
+            }
+        }
+        data_filename_ = "data/"+filename;
+
+        std::cout << "Started data log file: " << boost::filesystem::current_path().string() << "/" << data_filename_;
+
+        // open file and add header
+        data_file_.open(data_filename_.c_str());
+
+        // write header
+        //             12345678901234567890123456789012345678901234567890123456789012345678901234567890
+        data_file_ << "%%       1) GPS Time 2) Prn  3) Pseudorange  4) Prn  5) Pseudorange ..." << std::endl;
+
+    } catch (std::exception &e) {
+        std::cout << "Error opening log file: " << e.what();
+        if (data_file_.is_open())
+            data_file_.close();
+        return false;
+    }
+    return true;
+}
+
+void RxmRawCallback(ublox::RawMeas raw_meas, double time_stamp) {
+    try {
+
+        data_file_ << fixed << setw(20) << setprecision(3) << (double)raw_meas.iTow;
+
+        for(int ii=0;ii<raw_meas.numSV; ii++) {
+            data_file_  << setw(20) << raw_meas.rawmeasreap[ii].svid  
+                        << setw(20) << raw_meas.rawmeasreap[ii].psuedorange; // m
+        }
+
+        data_file_ << std::endl;
+
+    } catch (std::exception &e) {
+        std::cout << "RxmRawCallback() error"
+    }    
+}
+
+void StopLoggingData() {
+    if (data_file_.is_open())
+        data_file_.close();
+}
+
+int main(int argc, char **argv)
+{
+    Ublox my_gps;
+
+    if(argc < 3) {
+        std::cerr << "Usage: ublox_example <serial port address> <baud rate>" << std::endl;
+        return 0;
+    }
+    std::string port(argv[1]);
+    int baudrate=115200;
+    istringstream(argv[2]) >> baudrate;
+
+    // Connect to Receiver
+    bool result = my_gps.Connect(port,baudrate);
+        if (result) {
+            cout << "Successfully connected." << endl;
+        }
+        else {
+            cout << "Failed to connect." << endl;
+            return -1;
+        }
+    //! Start Data Logging
+    bool logging_on = StartDataLogging("range_data.log")
+
+    // Set Callback for pseudorange data
+    my_gps.set_rxm_raw_callback(RxmRawCallback);
+
+    //! Configure ublox
+    // request pseudorange data
+    my_gps.ConfigureMessageRate(0x02,0x10,1);
+
+
+    // loop forever
+    while(1)
+      usleep(50*1000); // sleep for 50 ms
+
+    my_gps.Disconnect();
+    StopLoggingData();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
